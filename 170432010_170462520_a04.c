6dd466de714dea33f0eab3ae077a2c71491ebb25
Matthew D <matdiet@hotmail.com>
2020-07-27 18:30:51 -0400

    Created base C file to work in going forward

---
+++ 170432010_170462520_a04.c
@@ -0,0 +1,16 @@
+/*
+Repository URL: https://github.com/ThePark0ur/CP386A4
+
+Authors:
+Kamran Tayyab       170432010   Git: Kamran14
+Matthew Dietrich    170462520   Git: ThePark0ur
+*/
+
+//Include Statements
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+//#include <pthread.h>
+//#include <semaphore.h>
+
